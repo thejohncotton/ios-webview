9288d245e1fe684369bfd9d5f9c0d0eebc77528f
John Cotton <john@1080people.com>
2018-05-03 23:03:16 -0400

    Initial Commit

---
+++ web-view/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  web-view
+//
+//  Created by John Cotton on 5/3/18.
+//  Copyright © 2018 John Cotton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
