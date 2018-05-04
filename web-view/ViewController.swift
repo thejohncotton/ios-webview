2571087eca858e6d7187629178222ef3a4f2adbe
John Cotton <john@1080people.com>
2018-05-03 23:05:53 -0400

    initialize with web viewstoryboard and vc

--- web-view/ViewController.swift
+++ web-view/ViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ViewController.swift
-//  web-view
+//  AF Turnout Gear Selector
 //
 //  Created by John Cotton on 5/3/18.
 //  Copyright © 2018 John Cotton. All rights reserved.
@@ -9,17 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var webView: UIWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let url = NSURL (string: "https://apple.com");
+        let requestObj = NSURLRequest(url: url! as URL);
+        webView.loadRequest(requestObj as URLRequest);
+        
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
+
 
